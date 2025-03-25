42817091677c52801993ca7cdd292aa0dc92b611
Maarten Duijndam <m.j.a.duijndam@uu.nl>
2025-03-25 15:11:15 +0100

    Init new repo

---
+++ fill_in_corrects.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+
+import csv
+import sys
+import os
+import os.path as p
+from argparse import ArgumentParser
+
+PathHint = str | bytes | os.PathLike
+
+CUE = "cue"
+RESP = "response"
+NL = "NL"
+EN = "EN"
+NL_NAME = "nl_name"
+EN_NAME = "en_name"
+ACC = "accuracy"
+NA = "NA"
+
+SEP = ","
+
+
+def fillin_corrects(trials: list[dict]) -> list[dict]:
+    """Fills in 1 when correct, NA when . is in the response, leave
+    empty otherwise"""
+    for trial in trials:
+        resp = trial[RESP]
+        if resp == ".":
+            trial[ACC] = NA
+            continue
+
+        if trial[CUE] == NL and resp == trial[NL_NAME]:
+            trial[ACC] = 1
+        elif trial[CUE] == EN and resp == trial[EN_NAME]:
+            trial[ACC] = 1
+
+
+def open_files(fn: PathHint, suffix: PathHint) -> None:
+    """Opens the in- and output and corrects the files, the infile
+    must exist and the output based on the suffix shouldn't.
+    """
+
+    base, ext = os.path.splitext(fn)
+    outfn = base + suffix + ext
+
+    if not p.exists(fn):
+        raise RuntimeError(f"Oops the input {fn} doesn't exist")
+    if p.exists(outfn):
+        raise RuntimeError(
+            f"Oops the output {outfn} already exist, I'm not overwriting it..."
+        )
+
+    rows = []
+
+    with open(fn, "r") as input:
+        reader = csv.DictReader(
+            input,
+        )
+        rows = [row for row in reader]
+
+    fillin_corrects(rows)
+
+    with open(outfn, "w") as output:
+        writer = csv.DictWriter(output, fieldnames=rows[0].keys())
+        writer.writerows(rows)
+
+
+def main():
+    """Run the program"""
+    parser = ArgumentParser(
+        "fill_in_corrects", "run this on a csv file, to fill in the corrects"
+    )
+    parser.add_argument("files", nargs="+", type=str, help="The input files")
+
+    parser.add_argument(
+        "-s", "--suffix", help="Determines the output name", default="_out"
+    )
+
+    args = parser.parse_args()
+
+    for infile in args.files:
+        open_files(infile, args.suffix)
+
+
+if __name__ == "__main__":
+    main()
